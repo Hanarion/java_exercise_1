459d1f961fde7e5d0b4143138737a2d6bd9b3da0
Hanarion <landryjuge@gmail.com>
2022-02-10 13:59:26 +0100

    I'm a scatmeeeeeen

--- src/com/landryjuge/HelloWorld.java
+++ src/com/landryjuge/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.landryjuge;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
     }
 }
