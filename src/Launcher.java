70c92c501d081fdc3567688266149687039edb37
Hanarion <landryjuge@gmail.com>
2022-02-10 14:09:38 +0100

    I'm blue

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class Launcher {
+
+    public static String askCommand(Scanner scanner) {
+        String entree = scanner.next();
+        if (!entree.equals("quit")) {
+            System.out.println("Unknown command");
+            return entree;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the not so great Java program, today we are going to do ABSOLUTELY NOTHING !");
         Scanner toto = new Scanner(System.in);
-        String entree = toto.next();
-        if (!entree.equals("quit")){
-            System.out.println("Unknown command");
-            return;
-        }
+        while (askCommand(toto) != null) ;
+        toto.close();
     }
 }
